220d8eb0863919d83e24f58e1ec02bb0602bc8c5
Axel Vergara <a20110341@ceti.mx>
2024-08-13 09:39:21 -0600

    IosCalculator

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {StatusBar, Text, View} from 'react-native';
+import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+import {globalStyles} from './config/theme/app-theme';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App() {
+  return (
+    <View style={globalStyles.background}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+export default App;
---
+++ src/presentation/hooks/useCalculator.tsx
@@ -0,0 +1,171 @@
+import {useEffect, useRef, useState} from 'react';
+
+enum Operator {
+  add = '+',
+  substract = '-',
+  multiply = '×',
+  divide = '÷',
+}
+
+export const useCalculator = () => {
+  const [formula, setFormula] = useState('0');
+  const [number, setNumber] = useState('0');
+  const [prevNumber, setPrevNumber] = useState('0');
+
+  const lastOperation = useRef<Operator>();
+
+  useEffect(() => {
+    if (lastOperation.current) {
+      const firstFormulaPart = formula.split(' ').at(0);
+      setFormula(`${firstFormulaPart} ${lastOperation.current} ${number}`);
+    } else {
+      setFormula(number);
+    }
+  }, [number]);
+
+  useEffect(() => {
+    const subResult = calculateSubResult();
+    setPrevNumber(`${subResult}`)
+  }, [formula])
+  
+
+  const clean = () => {
+    setNumber('0');
+    setPrevNumber('0');
+    lastOperation.current = undefined;
+    setFormula('0');
+  };
+
+  //borrar ultimo numero ingresdo
+  const deleteOperation = () => {
+    let currentSign = '';
+    let temporalNumber = number;
+
+    if (number.includes('-')) {
+      currentSign = '-';
+      temporalNumber = number.substring(1); //number.replace('-', '')
+    }
+
+    if (temporalNumber.length > 1) {
+      return setNumber(currentSign + temporalNumber.slice(0, -1));
+    }
+    setNumber('0');
+  };
+
+  const toggleSign = () => {
+    if (number.includes('-')) {
+      return setNumber(number.replace('-', ''));
+    }
+    setNumber('-' + number);
+  };
+
+  const buildNumber = (numberString: string) => {
+    if (number.includes('.') && numberString === '.') return;
+
+    if (number.startsWith('0') || number.startsWith('-0')) {
+      //punto decimal
+      if (numberString === '.') {
+        return setNumber(number + numberString);
+      }
+
+      //evaluar si es otro cero y no hay punto decimal
+      if (numberString === '0' && number.includes('.')) {
+        return setNumber(number + numberString);
+      }
+
+      //evaluar si es diferente de cero, no hay punto decimal y es el primer numero
+      if (numberString !== '0' && !number.includes('.')) {
+        return setNumber(numberString);
+      }
+      //evaluar para evitar el 000000
+      if (numberString === '0' && !number.includes('.')) {
+        return;
+      }
+      return setNumber(number + numberString);
+    }
+
+    setNumber(number + numberString);
+  };
+
+  const setLastNumber = () => {
+    calculateResult();
+    
+    if (number.endsWith('.')) {
+      setPrevNumber(number.slice(0, -1));
+    } else {
+      setPrevNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const divideOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.divide;
+  };
+
+  const multiplyOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.multiply;
+  };
+
+  const addOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.add;
+  };
+
+  const substractOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.substract;
+  };
+
+  const calculateResult = () => {
+    const result = calculateSubResult();
+    setFormula(`${result}`);
+
+    lastOperation.current = undefined;
+    setPrevNumber('0');
+  };
+
+  const calculateSubResult = (): number => {
+    const [firstValue, operation, secondValue] = formula.split(' ');
+
+    const num1 = Number(firstValue);
+    const num2 = Number(secondValue);
+
+    if (isNaN(num2)) return num1;
+
+    switch (operation) {
+      case Operator.add:
+        return num1 + num2;
+
+      case Operator.substract:
+        return num1 - num2;
+
+      case Operator.multiply:
+        return num1 * num2;
+
+      case Operator.divide:
+        return num1 / num2;
+
+      default:
+        throw new Error('Operation not implemented');
+    }
+  };
+
+  return {
+    //properties
+    number,
+    prevNumber,
+    formula,
+    //methods
+    buildNumber,
+    toggleSign,
+    clean,
+    deleteOperation,
+    divideOperation,
+    multiplyOperation,
+    addOperation,
+    substractOperation,
+    calculateResult,
+  };
+};
